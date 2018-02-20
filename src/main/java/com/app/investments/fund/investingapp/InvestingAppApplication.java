df1bb9f8b84dee385efd6d91cb3fe5bd2fc3abdc
Krzysztof Lakomy <krzysztof.lakomy@eo.pl>
2018-02-20 12:03:28 +0100

    Project initial commit

---
+++ src/main/java/com/app/investments/fund/investingapp/InvestingAppApplication.java
@@ -0,0 +1,12 @@
+package com.app.investments.fund.investingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestingAppApplication.class, args);
+	}
+}
